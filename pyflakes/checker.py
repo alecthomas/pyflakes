f703bbb96ccc6fea94109f32fee16f59962d6439
Alec Thomas <alec@swapoff.org>
2010-08-27 15:38:00 +1000

    Don't report locals named "unused_*" or "_" as unused.

--- pyflakes/checker.py
+++ pyflakes/checker.py
@@ -118,7 +118,8 @@ class Checker(ast.NodeVisitor):
             upscope = self.scope
             for name, value in scope.iteritems():
                 is_method_self = name == 'self' and isinstance(upscope, ClassScope)
-                if not value.used and not is_method_self:
+                unused_name = name.startswith('unused_') or not name.strip('_')
+                if not unused_name and not value.used and not is_method_self:
                     self.report(messages.UnusedLocal, value.source.lineno, value.source.col_offset, name)
         self.dead_scopes.append(scope)
 
